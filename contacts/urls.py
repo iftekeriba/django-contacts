812204c31356bd892bfbbe0c0ca3c4d8ef456218
JBeCast <jbcsound@gmail.com>
2014-07-30 05:54:29 -0300

    Created ContactListView and corrected its template. Updated app urlconf.

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from contacts import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.ContactListView.as_view(), name='list'),
     url(r'^create/$', views.ContactCreateView.as_view(), name='create'),
 )
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,21 @@
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse
 
 from .models import Contact
+
+
+class ContactListView(ListView):
+    model = Contact
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactListView, self).get_context_data(**kwargs)
+        return context
 
 
 class ContactCreateView(CreateView):
     model = Contact
     template_name_suffix = '_create'
+
+    def get_success_url(self):
+        return reverse('contacts:list')
