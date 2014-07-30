e099ea309885c217d6eb34c539b82c9fb43c9be2
JBeCast <jbcsound@gmail.com>
2014-07-30 01:06:10 -0300

    Corrected app urlconf.

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from contacts import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.ContactCreateView.as_view(), name='create'),
+    url(r'^create/$', views.ContactCreateView.as_view(), name='create'),
 )
