446a5faf1d568c11e4f2a83c3c2fdc909eecb71e
JBeCast <jo.berm.cast@gmail.com>
2014-08-01 07:56:00 -0300

    Added contact_detail template.

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.core.urlresolvers import reverse
+from django.forms.models import model_to_dict
 
 from .models import Contact
 
@@ -17,8 +18,9 @@ class ContactDetailView(DetailView):
     model = Contact
 
     def get_context_data(self, **kwargs):
-        context = super(ContactDetailView, self).get_context_data(**kwargs)
-        context['fields'] = Contact._meta.fields
+        # context = super(ContactDetailView, self).get_context_data(**kwargs)
+        instance = Contact.objects.get(pk=1)
+        context = model_to_dict(instance)
         return context
 
 
